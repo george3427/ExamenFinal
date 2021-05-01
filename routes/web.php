27d2c08b51ee309ae0f43ceef4b162890ead4b84
George Fernandez <georgefernandez3427@gmail.com>
2021-05-01 16:01:38 -0600

    ruteado con prefijos

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,24 @@
 |
 */
 
-
-
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', 'IndexController@index');
 });
 
+
+Route::group(['prefix' => 'post','middleware' => 'auth'], function () {
+    Route::get('/nuevo', 'UserPostController@nuevo');
+    Route::post('/nuevo', 'UserPostController@postNuevo');
+    Route::get('/ver/{post_id}', 'UserPostController@ver');
+    Route::get('/editar/{post_id}', 'UserPostController@editar');
+    Route::post('/editar/{post_id}', 'UserPostController@postEditar');
+    Route::get('/eliminar/{post_id}', 'UserPostController@eliminarPost');
+});
+
+Route::group(['prefix' => 'wiki','middleware' => 'auth'], function () {
+    Route::get('/index', 'WikiController@index');
+    Route::get('/api/get', 'WikiController@apiGet');
+});
 
 
 Auth::routes();
