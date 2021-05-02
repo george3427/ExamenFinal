ca6c9ea6121c4801253a6811735de1f5528c0d45
George Fernandez <georgefernandez3427@gmail.com>
2021-05-02 10:27:36 -0600

    Seeder fake data

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -24,5 +24,34 @@ class AdminSeeder extends Seeder
             'user_id' => 1,
             'rol_tipo' => 'admin',
         ]);
+
+        DB::table('users')->insert([
+            'id' => 2,
+            'name' => 'Editor',
+            'email' => 'editor@editor.com',
+            'password' => bcrypt('123456'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('roles')->insert([
+            'user_id' => 2,
+            'rol_tipo' => 'editor',
+        ]);
+
+
+        DB::table('users')->insert([
+            'id' => 3,
+            'name' => 'Usuario',
+            'email' => 'usuario@usuario.com',
+            'password' => bcrypt('123456'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('roles')->insert([
+            'user_id' => 3,
+            'rol_tipo' => 'usuario',
+        ]);
     }
 }
---
+++ database/seeds/PostSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0; $i < 5; $i++) { 
+            DB::table('user_posts')->insert([
+                "titulo" => 'Lorem ipsum seeder example',
+                "contenido" => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+                "user_id" => 1
+            ]);
+        }
+
+        for ($i=0; $i < 5; $i++) { 
+            DB::table('user_posts')->insert([
+                "titulo" => 'Lorem ipsum seeder example',
+                "contenido" => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+                "user_id" => 2
+            ]);
+        }
+
+        for ($i=0; $i < 5; $i++) { 
+            DB::table('user_posts')->insert([
+                "titulo" => 'Lorem ipsum seeder example',
+                "contenido" => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
+                "user_id" => 3
+            ]);
+        }
+        
+    }
+}
